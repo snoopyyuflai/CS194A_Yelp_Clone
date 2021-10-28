bf5b687ae666ba5e8aadc06165acd28ff4e74b2d
LAPTOP-P0J1M0TC\raes2 <yuflai@stanford.edu>
2021-10-27 18:21:40 -0700

    Finished part 5

--- app/src/main/java/edu/stanford/yuflai/assignment3/MainActivity.kt
+++ app/src/main/java/edu/stanford/yuflai/assignment3/MainActivity.kt
@@ -2,10 +2,43 @@ package edu.stanford.yuflai.assignment3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val TAG = "MainActivity"
+private const val API_KEY = "ntBEsTDXOM7wDZT8qB97vxkJ7nzzvCGzCp525TphsWwDypgKomkAF5jq1nPvPgBJd-CgUcUl0PoDQpeX457KyYlPyAOYn4fGIFJGnMd1C0GBhC0bOTT6I5eGht55YXYx"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val restaurants = mutableListOf<YelpRestaurant>()
+        val adapter = RestaurantsAdapter(this, restaurants)
+        rvRestaurants.adapter = adapter
+        rvRestaurants.layoutManager = LinearLayoutManager(this)
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        val yelpService = retrofit.create(YelpService::class.java)
+        yelpService.searchRestaurants("Bearer $API_KEY","Avocado Toast","New York").enqueue(object : Callback<YelpSearchResult>{
+            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
+                Log.i(TAG, "onResponse $response")
+                val body = response.body()
+                if(body == null){
+                    Log.w(TAG, "Did not receive valid response body form Yelp API... exiting")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.i(TAG, "onResponse $t")
+            }
+        })
+
     }
 }
