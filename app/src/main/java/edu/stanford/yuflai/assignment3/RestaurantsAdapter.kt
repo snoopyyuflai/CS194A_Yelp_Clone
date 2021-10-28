e1f58b0826b68c77fbbeca7b1516c402790cf1a5
LAPTOP-P0J1M0TC\raes2 <yuflai@stanford.edu>
2021-10-27 18:51:22 -0700

    Finished Basic Implementation

--- app/src/main/java/edu/stanford/yuflai/assignment3/RestaurantsAdapter.kt
+++ app/src/main/java/edu/stanford/yuflai/assignment3/RestaurantsAdapter.kt
@@ -2,9 +2,14 @@ package edu.stanford.yuflai.assignment3
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_restaurant.view.*
 
 class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaurant>) :
@@ -24,6 +29,15 @@ class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaur
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         fun bind(restaurant: YelpRestaurant){
             itemView.tvName.text = restaurant.name
+            itemView.ratingBar.rating = restaurant.rating.toFloat()
+            itemView.tvNumReviews.text = "${restaurant.numReviews} Reviews"
+            itemView.tvAddress.text = restaurant.location.address
+            itemView.tvCategory.text = restaurant.categories[0].title
+            itemView.tvDistance.text = restaurant.displayDistance()
+            itemView.tvPrice.text = restaurant.price
+            Glide.with(context).load(restaurant.imageUrl).apply(RequestOptions().transforms(
+                CenterCrop(), RoundedCorners(20)
+            )).into(itemView.imageView)
         }
 
     }
